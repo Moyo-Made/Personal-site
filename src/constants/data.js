70a408e07f201a925ea9876ccd698216b890409b
Moyomade <adegbitemoyomade2004@gmail.com>
2024-04-14 01:16:14 +0000

    Added constants

---
+++ src/constants/data.js
@@ -0,0 +1,48 @@
+import megaImage from "../assets/megaleague.png";
+import chatappImage from "../assets/chat.png";
+import sushiImage from "../assets/sushi.png";
+import hoobankImage from "../assets/hoobank.png";
+import realEstateImage from "../assets/real estate.png";
+
+export const data = [
+	{
+		id: 1,
+		title: "MEGA League Website",
+		image: megaImage,
+		desc: "During my internship, I worked closely with the design and backend teams as a front-end developer. We revamped the Mega League website using Vue.js. This project aimed to unite high-school students worldwide to address social issues through technology.",
+		github: "https://github.com/MEGA-Hackathon/kinect.megahack.tech",
+		demo: "https://megahack.tech/",
+	},
+	{
+		id: 2,
+		image: chatappImage,
+		desc: "I made a chat app with React and Firebase, combining frontend and real-time database features. It provides smooth messaging using React's structure and Firebase's secure backend. This project highlights my skills in web development, focusing on usability and performance.",
+		title: "Real-Time Chat App",
+		github: "https://github.com/Moyo-Made/CHAT-APP",
+		demo: "https://meetup7.netlify.app",
+	},
+	{
+		id: 3,
+		image: sushiImage,
+		title: "Sushi Website",
+		desc: "I focused on creating a visually appealing layout that showcases the beauty of sushi while ensuring a smooth user experience. Leveraging React's component-based architecture and Tailwind CSS's flexibility, the website offers both functionality and aesthetic appeal.",
+		github: "https://github.com/Moyo-Made/SUSHI",
+		demo: "https://getsushi.netlify.app/",
+	},
+	{
+		id: 4,
+		title: "Hoobank",
+		image: hoobankImage,
+		desc: "HooBank, a modern banking application built with React.js, provides a user-friendly interface for managing finances. Its clean design, powered by Tailwind CSS, offers a seamless experience across devices. I built HooBank using React components, ensuring a smooth and responsive user experience for modern banking on the go.",
+		github: "https://github.com/Moyo-Made/Modern-banking-app",
+		demo: "https://hoobankss.netlify.app/",
+	},
+	{
+		id: 5,
+		image: realEstateImage,
+		title: "Real Estate Website",
+		desc: "This React.js real estate website lets users browse properties with detailed information and photos. Search functionality helps them filter listings based on needs. The responsive design ensures a smooth experience on any device. My contributions include building the UI, implementing functionalities, and ensuring responsiveness.",
+		github: "https://github.com/Moyo-Made/REAL-ESTATE-WEBSITE",
+		demo: "https://real-estate-websitee.netlify.app/",
+	},
+];
---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
