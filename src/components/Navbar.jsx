760a1560630cedeb7c1518d55738ea0ea06a01b6
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-05-04 23:09:47 +0100

    Updated navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,33 @@ import { navLinks } from "../constants";
 import logo from "/logo.png";
 import close from "../assets/close.svg";
 import menu from "../assets/menu.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
 	const [toggle, setToggle] = useState(false);
+	const [scrolling, setScrolling] = useState(false);
+
+	useEffect(() => {
+	  const handleScroll = () => {
+		if (window.scrollY > 50) {
+		  setScrolling(true);
+		} else {
+		  setScrolling(false);
+		}
+	  };
+  
+	  window.addEventListener("scroll", handleScroll);
+  
+	  return () => {
+		window.removeEventListener("scroll", handleScroll);
+	  };
+	}, []);
 	return (
-		<nav className="w-full justify-evenly items-center flex py-4 shadow-md">
+		<nav
+      className={`fixed top-0 z-10 w-full bg-white justify-evenly items-center flex py-4 shadow-md ${
+        scrolling ? "bg-transparent" : "bg-opacity-100"
+      } transition-all duration-300`}
+    >
 			<div className="flex items-center md:ml-40">
 				<img src={logo} alt="logo" className="w-[30px] h-[20px]  ml-3" />
 				<span className="font-medium text-[18px] ml-2">Moyomade</span>
@@ -22,7 +43,7 @@ const Navbar = () => {
 						} mr-10`}
 						key={navLink.id}
 					>
-						<a href={`#${navLink.id}`}>{navLink.title}</a>
+						<a href={`#${navLink.id}`} className="scroll-link">{navLink.title}</a>
 					</li>
 				))}
 			</ul>
@@ -52,7 +73,7 @@ const Navbar = () => {
 								className={`font-poppins font-normal hover:font-bold cursor-pointer
 								text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mb-4"} text-white mb-4`}
 							>
-								<a href={`#${navLink.id}`}>{navLink.title}</a>
+								<a href={`#${navLink.id}`} className="scroll-link">{navLink.title}</a>
 							</li>
 						))}
 					</ul>
