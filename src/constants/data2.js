b7eeadde043f92ccc452751bbfd5a964b25a8807
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-05-07 04:38:25 +0100

    Updated constants

---
+++ src/constants/data2.js
@@ -0,0 +1,30 @@
+import sushiImage from "../assets/sushi.png";
+import hoobankImage from "../assets/hoobank.png";
+import realEstateImage from "../assets/real estate.png";
+
+export const data = [
+	{
+		id: 3,
+		image: sushiImage,
+		title: "Sushi Website",
+		desc: "I focused on creating a visually appealing layout that showcases the beauty of sushi while ensuring a smooth user experience. Leveraging React's component-based architecture and Tailwind CSS's flexibility, the website offers both functionality and aesthetic appeal.",
+		github: "https://github.com/Moyo-Made/SUSHI",
+		demo: "https://getsushi.netlify.app/",
+	},
+	{
+		id: 4,
+		title: "Hoobank",
+		image: hoobankImage,
+		desc: "HooBank, a modern banking application built with React.js, provides a user-friendly interface for managing finances. Its clean design, powered by Tailwind CSS, offers a seamless experience across devices. I built HooBank using React components, ensuring a smooth and responsive user experience for modern banking on the go.",
+		github: "https://github.com/Moyo-Made/Modern-banking-app",
+		demo: "https://hoobankss.netlify.app/",
+	},
+	{
+		id: 5,
+		image: realEstateImage,
+		title: "Real Estate Website",
+		desc: "This React.js real estate website lets users browse properties with detailed information and photos. Search functionality helps them filter listings based on needs. The responsive design ensures a smooth experience on any device. My contributions include building the UI, implementing functionalities, and ensuring responsiveness.",
+		github: "https://github.com/Moyo-Made/REAL-ESTATE-WEBSITE",
+		demo: "https://real-estate-websitee.netlify.app/",
+	},
+];
