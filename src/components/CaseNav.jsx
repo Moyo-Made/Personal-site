12f70bd0de57f73903dd815c02086a6b472d793e
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-05-10 18:06:36 -0700

    feat: added navbar for case study

--- src/components/CaseNav.jsx
+++ src/components/CaseNav.jsx
@@ -3,24 +3,24 @@ import Pages from "../Pages";
 import { FaHome } from "react-icons/fa";
 
 const CaseNav = () => {
-  return (
-    <div className="flex justify-center bg-white w-full fixed top-0 z-10">
-      <div className="flex space-x-2">
-        <Link to="/" element={<Pages />}>
-          <div className="">
-            <button>
-              <FaHome className="mt-6" />
-            </button>
-          </div>
-        </Link>
-        <Link to="/" element={<Pages />}>
-          <div className="mt-5">
-            <span>Back to home</span>
-          </div>
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex justify-center bg-white w-full fixed top-0 z-10 ">
+			<div className="flex space-x-2">
+				<Link to="/" element={<Pages />}>
+					<div className="">
+						<button>
+							<FaHome className="mt-6" />
+						</button>
+					</div>
+				</Link>
+				<Link to="/" element={<Pages />}>
+					<div className="mt-5">
+						<span>Back to home</span>
+					</div>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default CaseNav;
