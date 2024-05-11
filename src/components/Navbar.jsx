8f7748eb19821bdd78c754130ea717934877bb28
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-05-10 18:08:34 -0700

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,6 @@ import logo from "/logo.png";
 import close from "../assets/close.svg";
 import menu from "../assets/menu.svg";
 import { useEffect, useState } from "react";
-import Pages from "../Pages";
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
