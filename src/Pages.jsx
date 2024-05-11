f84da2c0957fc88fab6f4ed70720340698f6cf78
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-05-10 18:09:17 -0700

    updated pages.jsx

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -1,5 +1,5 @@
 import About from "./components/About";
-import ContactForm from "./components/ContactForm";
+// import ContactForm from "./components/ContactForm";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
@@ -7,17 +7,17 @@ import Services from "./components/Services";
 import Skills from "./components/Skills";
 
 const Pages = () => {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <About />
-      <Portfolio />
-      <Skills />
-      <Services />
-      <ContactForm />
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<Hero />
+			<About />
+			<Portfolio />
+			<Skills />
+			<Services />
+			{/* <ContactForm /> */}
+		</div>
+	);
 };
 
 export default Pages;
