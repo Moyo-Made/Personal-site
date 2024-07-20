3270741009cd63689083818e0d133aa2e9fb93ff
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-20 20:54:23 +0100

    fix: made some chnages to data.js

--- src/constants/data2.js
+++ src/constants/data2.js
@@ -2,9 +2,11 @@ import sushiImage from "../assets/sushi.png";
 import hoobankImage from "../assets/hoobank.png";
 import realEstateImage from "../assets/real estate.png";
 
+
 export const data = [
+	
 	{
-		id: 3,
+		id: 4,
 		image: sushiImage,
 		title: "Sushi Website",
 		desc: "I focused on creating a visually appealing layout that showcases the beauty of sushi while ensuring a smooth user experience. Leveraging React's component-based architecture and Tailwind CSS's flexibility, the website offers both functionality and aesthetic appeal.",
@@ -12,7 +14,7 @@ export const data = [
 		demo: "https://getsushi.netlify.app/",
 	},
 	{
-		id: 4,
+		id: 5,
 		title: "Hoobank",
 		image: hoobankImage,
 		desc: "HooBank, a modern banking application built with React.js, provides a user-friendly interface for managing finances. Its clean design, powered by Tailwind CSS, offers a seamless experience across devices. I built HooBank using React components, ensuring a smooth and responsive user experience for modern banking on the go.",
@@ -20,7 +22,7 @@ export const data = [
 		demo: "https://hoobankss.netlify.app/",
 	},
 	{
-		id: 5,
+		id: 6,
 		image: realEstateImage,
 		title: "Real Estate Website",
 		desc: "This React.js real estate website lets users browse properties with detailed information and photos. Search functionality helps them filter listings based on needs. The responsive design ensures a smooth experience on any device. My contributions include building the UI, implementing functionalities, and ensuring responsiveness.",
