e4cc7888b841a4c8104b30e5b59f879303853bc1
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-20 20:54:14 +0100

    fix: made some chnages to data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,8 +1,6 @@
 import megaImage from "../assets/megaleague.png";
 import chatappImage from "../assets/chat.png";
-import sushiImage from "../assets/sushi.png";
-import hoobankImage from "../assets/hoobank.png";
-import realEstateImage from "../assets/real estate.png";
+import promtopia from "../assets/promptopia.png";
 
 export const data = [
 	{
@@ -15,6 +13,14 @@ export const data = [
 	},
 	{
 		id: 2,
+		image: promtopia,
+		title: "Promptopia - Discover, Create, and Share Creative Prompts with AI",
+		desc: "Promptopia is an innovative, open-source AI prompting tool designed to inspire creativity in the modern world. Whether you're a writer, artist, or simply someone looking for a spark of inspiration, Promptopia offers a dynamic platform to discover, create, and share imaginative prompts.",
+		github: "https://github.com/Moyo-Made/Promptopia.git",
+		demo: "https://promptopia-peach-one.vercel.app/",
+	},
+	{
+		id: 3,
 		image: chatappImage,
 		desc: "I made a chat app with React and Firebase, combining frontend and real-time database features. It provides smooth messaging using React's structure and Firebase's secure backend. This project highlights my skills in web development, focusing on usability and performance.",
 		title: "Real-Time Chat Application",
@@ -22,28 +28,3 @@ export const data = [
 		demo: "https://meetup7.netlify.app",
 	},
 ]
-// 	{
-// 		id: 3,
-// 		image: sushiImage,
-// 		title: "Sushi Website",
-// 		desc: "I focused on creating a visually appealing layout that showcases the beauty of sushi while ensuring a smooth user experience. Leveraging React's component-based architecture and Tailwind CSS's flexibility, the website offers both functionality and aesthetic appeal.",
-// 		github: "https://github.com/Moyo-Made/SUSHI",
-// 		demo: "https://getsushi.netlify.app/",
-// 	},
-// 	{
-// 		id: 4,
-// 		title: "Hoobank",
-// 		image: hoobankImage,
-// 		desc: "HooBank, a modern banking application built with React.js, provides a user-friendly interface for managing finances. Its clean design, powered by Tailwind CSS, offers a seamless experience across devices. I built HooBank using React components, ensuring a smooth and responsive user experience for modern banking on the go.",
-// 		github: "https://github.com/Moyo-Made/Modern-banking-app",
-// 		demo: "https://hoobankss.netlify.app/",
-// 	},
-// 	{
-// 		id: 5,
-// 		image: realEstateImage,
-// 		title: "Real Estate Website",
-// 		desc: "This React.js real estate website lets users browse properties with detailed information and photos. Search functionality helps them filter listings based on needs. The responsive design ensures a smooth experience on any device. My contributions include building the UI, implementing functionalities, and ensuring responsiveness.",
-// 		github: "https://github.com/Moyo-Made/REAL-ESTATE-WEBSITE",
-// 		demo: "https://real-estate-websitee.netlify.app/",
-// 	},
-// ];
