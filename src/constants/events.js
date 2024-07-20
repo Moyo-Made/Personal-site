b610862a01dfc34e623f22f32d088e119fa982bd
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-20 20:58:40 +0100

    added file for event highlights

---
+++ src/constants/events.js
@@ -0,0 +1,15 @@
+import Cavista1 from "../assets/cavista1.jpg";
+import Cavista2 from "../assets/cavista2.jpg";
+import Cavista3 from "../assets/cavista3.jpg";
+
+const eventData = [
+	{
+		id: 1,
+		title: "Cavista Hackathon",
+		description:
+			"Participating in the Cavista Hackathon was an exhilarating experience where I collaborated with a diverse team of five, including designers, project managers, and fellow developers, to develop innovative ideas within a challenging 48-hour timeframe. This event was a true test of my skills, pushing me to adapt quickly and work effectively with a new group for the first time. Throughout the hackathon, I forged strong connections with other developers, sharing our experiences and learning from one another. Our team worked tirelessly, pitching our product to the judges with confidence and enthusiasm. Beyond the intense coding sessions, we also enjoyed moments of relaxation and exercise, which helped us stay energized and focused. Overall, the Cavista Hackathon was a rewarding experience that not only showcased my technical abilities but also enhanced my teamwork and networking skills.",
+		images: [Cavista1, Cavista2, Cavista3],
+	},
+];
+
+export default eventData;
