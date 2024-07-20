eb97189fe64ad8c4439d1da67b00cf396cae58dd
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-20 20:52:45 +0100

    fix: made some changes to th case study

--- src/components/CaseStudy.jsx
+++ src/components/CaseStudy.jsx
@@ -1,13 +1,15 @@
 import CaseStudy1 from "./CaseStudy1";
 import CaseStudy2 from "./CaseStudy2";
+import CaseStudy3 from "./CaseStudy3";
 
 const CaseStudy = () => {
-  return (
-    <div className="mt-12 overflow-x-hidden">
-      <CaseStudy1 />
-      <CaseStudy2 />
-    </div>
-  );
+	return (
+		<div className="mt-12 overflow-x-hidden">
+			<CaseStudy3 />
+			<CaseStudy1 />
+			<CaseStudy2 />
+		</div>
+	);
 };
 
 export default CaseStudy;
